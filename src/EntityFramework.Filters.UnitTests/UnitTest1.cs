a363271b299d47d527a9a7953afb66e28c0339d2
Patrick Wolfs <patrick.wolfs@netintellect.be>
2016-01-07 16:41:52 +0100

    Added a unit test project to play around

---
+++ src/EntityFramework.Filters.UnitTests/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using EntityFramework.Filters.Example;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EntityFramework.Filters.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Should_filter_based_on_global_value()
+        {
+            using (var context = new ExampleContext())
+            {
+                var tenant = context.Tenants.Find(1);
+                context.CurrentTenant = tenant;
+                context.EnableFilter("Tenant")
+                    .SetParameter("tenantId", tenant.TenantId);
+
+                Assert.IsTrue(context.BlogEntries.Count() == 1);
+            }
+        }
+    }
+}
